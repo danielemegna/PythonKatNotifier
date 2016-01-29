2db31dfea344f89b6c2920c260604d9853c230e9
Daniele Megna <megna.dany@gmail.com>
2016-01-29 21:47:44 +0100

    Refactoring of current version

--- test/KatPageTest.py
+++ test/KatPageTest.py
@@ -9,33 +9,47 @@ class KatPage:
     return
 
   def movies(self):
-    found = self.soup.find_all("a", class_="cellMainLink")
+    links = self.__extract_movie_links()
+    self.__sanitize_movie_links(links)
+    return self.__movie_links_to_titles_array(links)
 
+  def __movie_links_to_titles_array(self, links):
     movies = []
-    for movie in found:
-      movie.strong.unwrap()
+    for movie in links:
       movies.append(''.join(movie.contents))
-
     return movies
+
+  def __sanitize_movie_links(self, links):
+    for movie in self.__extract_movie_links():
+      movie.strong.unwrap()
+
+  def __extract_movie_links(self):
+    return self.soup.find_all("a", class_="cellMainLink")
 
 
 class KatPageTest(unittest.TestCase):
 
+  def setUp(self):
+    html = self.__read_file('test/katPage.html')
+    self.page = KatPage(html)
+
   def test_recognizeCorrectlyNumberOfMovies(self):
-    html = self.read_file('test/katPage.html')
-    page = KatPage(html)
-    movies = page.movies()
+    movies = self.page.movies()
     self.assertEqual(len(movies), 12)
 
   def test_readCorrectlyMovieTitles(self):
-    html = self.read_file('test/katPage.html')
-    page = KatPage(html)
-    movies = page.movies()
+    movies = self.page.movies()
     self.assertEqual(movies[0], # todo -> this will be 'movies[0].title'
       "Son Of A Gun (2014) BRrip XviD - Italian English Ac3 5 1 Sub ita eng MIRCrew"
     );
+    self.assertEqual(movies[1],
+      "Lo Stagista Inaspettato-The Intern 2015 iTALiAN BRRip XviD BLUWORLD"
+    );
+    self.assertEqual(movies[7],
+      "Inside Out 2015 iTALiAN BDRip XviD-TRL[MT] avi"
+    );
 
-  def read_file(self, path):
+  def __read_file(self, path):
     file = open(path, 'r')
     html = file.read()
     file.close()
