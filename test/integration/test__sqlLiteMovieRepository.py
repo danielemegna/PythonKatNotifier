99a3ef3409d056fe71c341da29bb5d05fc9ea1c3
Daniele Megna <daniele.megna@xpeppers.com>
2016-02-10 14:36:21 +0100

    Added copy temp db before integration tests

--- test/integration/test__sqlLiteMovieRepository.py
+++ test/integration/test__sqlLiteMovieRepository.py
@@ -1,17 +1,29 @@
 import unittest
 import os
+import shutil
 
 from katnotifier import SqlLiteMovieRepository
 
 class SqlLiteMovieRepositoryTest(unittest.TestCase):
-
+  
   def setUp(self):
-    currentDirectory = os.path.dirname(__file__)
-    dbPath = os.path.join(currentDirectory, 'test.db')
-    self.repository = SqlLiteMovieRepository(dbPath) 
+    dbPath = self.__absolutePathFromRelative('test.db')
+    tempDbPath = self.__absolutePathFromRelative('temp.db')
+    shutil.copyfile(dbPath, tempDbPath)
+    self.repository = SqlLiteMovieRepository(tempDbPath) 
+ 
+  def tearDown(self):
+    tempDbPath = self.__absolutePathFromRelative('temp.db')
+    os.remove(tempDbPath) 
+    
 
   def test_alreadyNotified(self):
     movies = self.repository.alreadyNotified()
     self.assertEquals(2, len(movies))
     self.assertEquals("Titolo film notificato", movies[0].title)
     self.assertEquals("Altro film notificato", movies[1].title)
+
+  def __absolutePathFromRelative(self, relativePath):
+    dirPath = os.path.dirname(__file__)
+    return os.path.join(dirPath, relativePath)
+    
