7f332e23dc25fa0448a1db3d746ef564fb02efd9
Daniele Megna <daniele.megna@xpeppers.com>
2016-02-11 14:35:04 +0100

    Developed first version of UrlLibHtmlRetriever

--- katnotifier/htmlRetriever.py
+++ katnotifier/htmlRetriever.py
@@ -1,4 +1,12 @@
 class HtmlRetriever:
 
-  def get(url):
+  def get(self, url):
     raise NotImplementedError("You're calling an abstract class!")
+
+import urllib2
+class UrlLibHtmlRetriever:
+
+  def get(self, url):
+    response = urllib2.urlopen(url)
+    return response.read()
+
