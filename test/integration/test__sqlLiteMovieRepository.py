01c037a8356b6950748479d9b1276492a3aeb426
Daniele Megna <daniele.megna@xpeppers.com>
2016-02-11 14:11:59 +0100

    Refactored test using Movie class

--- test/integration/test__sqlLiteMovieRepository.py
+++ test/integration/test__sqlLiteMovieRepository.py
@@ -20,8 +20,8 @@ class SqlLiteMovieRepositoryTest(unittest.TestCase):
   def test_alreadyNotified(self):
     movies = self.repository.alreadyNotified()
     self.assertEquals(2, len(movies))
-    self.assertEquals("Titolo film notificato", movies[0].title)
-    self.assertEquals("Altro film notificato", movies[1].title)
+    self.assertEquals(Movie("Titolo film notificato"), movies[0])
+    self.assertEquals(Movie("Altro film notificato"), movies[1])
 
   def test_add(self):
     movies = self.repository.add(Movie("Nuovo film da inserire"))
