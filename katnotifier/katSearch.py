6185aed5036e2c5f37e384fd6c3c460666d7c6dd
Daniele Megna <megna.dany@gmail.com>
2016-02-14 21:56:36 +0100

    Developing KatSearch class that generates search urls

--- katnotifier/katSearch.py
+++ katnotifier/katSearch.py
@@ -5,8 +5,22 @@ class KatSearch:
   protocol = "http"
   katDomain = "kat.cr"
 
-  def __init__(self, keywords):
-    self.keywords = keywords
+  def __init__(self):
+    self.includedWords = []
+    self.excludedWords = []
+
+  def include(self, word):
+    self.includedWords.append(word)
+    return self
+
+  def exclude(self, word):
+    self.excludedWords.append(word)
+    return self
 
   def toUrl(self):
-    return "{}://{}/usearch/{}".format(self.protocol, self.katDomain, urllib.quote(self.keywords))
+    keywords = " ".join(self.includedWords)
+    keywords += " "
+    keywords += " ".join(map(lambda word: "-"+word, self.excludedWords))
+    keywords = keywords.strip()
+    keywords = urllib.quote(keywords)
+    return "{}://{}/usearch/{}".format(self.protocol, self.katDomain, keywords)
--- test/unit/test__katSearch.py
+++ test/unit/test__katSearch.py
@@ -8,13 +8,27 @@ from katnotifier import KatSearch
 class KatSearchTest(UnitTestBase):
 
   def test_simpleKatSearch(self):
-    search = KatSearch("myTitle")
-    actual = search.toUrl()
+    search = KatSearch()  \
+      .include("myTitle")
     expected = "http://kat.cr/usearch/myTitle"
+
+    actual = search.toUrl()
     self.assertEquals(expected, actual)
 
   def test_multiKeywordsSearch(self):
-    search = KatSearch("searched movie title")
-    actual = search.toUrl()
+    search = KatSearch() \
+      .include("searched movie title")
     expected = "http://kat.cr/usearch/searched%20movie%20title"
+
+    actual = search.toUrl()
+    self.assertEquals(expected, actual)
+
+  def test_excludeSomeWords(self):
+    search = KatSearch()  \
+      .include("italian") \
+      .exclude("md")      \
+      .exclude("cam")
+    expected = "http://kat.cr/usearch/italian%20-md%20-cam"
+
+    actual = search.toUrl()
     self.assertEquals(expected, actual)
