958550c72220b210e342ad121d6e22f0395e4fea
Daniele Megna <megna.dany@gmail.com>
2017-02-18 15:27:59 +0100

    Removed MovieFilterPolicyList
    Develop it when you need it!

--- moviesnotifier/movieFilterPolicy.py
+++ moviesnotifier/movieFilterPolicy.py
@@ -4,13 +4,3 @@ class MovieFilterPolicy:
     return [m for m in movies if self._isInteresting(m)]
   def _isInteresting(self, movie):
     raise NotImplementedError("You're calling an abstract class!")
-
-class MovieFilterPolicyList(MovieFilterPolicy):
-
-  def __init__(self, policies):
-    self.policies = policies
-  def filter(self, movies):
-    raise NotImplementedError("Not yet implemented!")
-  def _isInteresting(self, movie):
-    raise NotImplementedError("Not yet implemented!")
-    #for p in self.policies:
