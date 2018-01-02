fb50dfe9ba5c26490e6859a505fabce3808fa1ae
Daniele Megna <megna.dany@gmail.com>
2018-01-02 12:34:47 +0100

    Solved CERTIFICATE_VERIFY_FAILED problem with corsaro
    ignore certificate validation

--- moviesnotifier/htmlRetriever.py
+++ moviesnotifier/htmlRetriever.py
@@ -3,13 +3,13 @@ class HtmlRetriever:
   def get(self, url):
     raise NotImplementedError("You're calling an abstract class!")
 
-import urllib2
-import gzip
+import urllib2, ssl, gzip
 from StringIO import StringIO
 class UrlLibHtmlRetriever:
 
   def get(self, url):
-    response = urllib2.urlopen(url)
+    context = ssl._create_unverified_context()
+    response = urllib2.urlopen(url, context=context)
     if response.info().get('Content-Encoding') == 'gzip':
         buf = StringIO(response.read())
         f = gzip.GzipFile(fileobj=buf)
