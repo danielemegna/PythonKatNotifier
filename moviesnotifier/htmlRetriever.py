6fbc0f87d4c729297df4cd74c4b23fd03ae832ea
Daniele Megna <megna.dany@gmail.com>
2018-06-29 19:38:04 +0200

    Added headers in UrlLibHtmlRetriever requests

--- moviesnotifier/htmlRetriever.py
+++ moviesnotifier/htmlRetriever.py
@@ -9,7 +9,15 @@ class UrlLibHtmlRetriever:
 
   def get(self, url):
     context = ssl._create_unverified_context()
-    response = urllib2.urlopen(url, context=context)
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+    }
+    req = urllib2.Request(url, headers=hdr)
+    response = urllib2.urlopen(req, context=context)
     if response.info().get('Content-Encoding') == 'gzip':
         buf = StringIO(response.read())
         f = gzip.GzipFile(fileobj=buf)
